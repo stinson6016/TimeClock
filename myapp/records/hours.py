4b5c662e55bd5e9a3250bf04d068f2979670b965
Patrick Stinson <stinson6016@gmail.com>
2024-03-28 11:55:22 -0400

    updating flagged on hours

--- myapp/records/hours.py
+++ myapp/records/hours.py
@@ -2,7 +2,7 @@ from datetime import date
 from flask import Blueprint, render_template, request
 from flask_login import login_required
 
-from .extra import quickSearch
+from .extra import quickSearch, searchFlagged
 from .webforms import SearchPunches
 
 from ..extra import getUsers
@@ -48,18 +48,33 @@ def portalsearch():
     user_hours = {}
     if employee:
         pull_users = Punch.query.join(Punch.user).where(Punch.clock_date >= first_day_search, Punch.clock_date <= last_day_search, Punch.user_id==employee).group_by(Users.name)
-        flagged = Punch.query.where(Punch.clock_date >= first_day_search, Punch.clock_date <= last_day_search, Punch.user_id==employee, Punch.flag=='y').count()
+        # flagged = Punch.query.where(Punch.clock_date >= first_day_search, Punch.clock_date <= last_day_search, Punch.user_id==employee, Punch.flag=='y').count()
     else:
         pull_users = Punch.query.join(Punch.user).where(Punch.clock_date >= first_day_search, Punch.clock_date <= last_day_search).group_by(Users.name)
-        flagged = Punch.query.where(Punch.clock_date >= first_day_search, Punch.clock_date <= last_day_search, Punch.flag=='y').count()
+        # flagged = Punch.query.where(Punch.clock_date >= first_day_search, Punch.clock_date <= last_day_search, Punch.flag=='y').count()
     for user in pull_users:
         user_hours[user.user_id] = getHours(user.user_id, first_day_search, last_day_search)
     return render_template('hours/hours-search.html',
                            form = form,
                            user_hours=user_hours,
                            pull_users=pull_users,
-                           flagged=flagged,
+                        #    flagged=flagged,
                            page='h',
+                           employee=employee,
+                           start=first_day_search,
+                           end=last_day_search)
+
+@hours.post('/getflagged')
+@login_required
+def getflagged():
+    start = request.args.get('start', default='')
+    end = request.args.get('end', default='')
+    employee = request.args.get('employee', default=None)
+    flag_count = searchFlagged(start, end, employee)
+    return render_template('punches/punches-flagged.html',
+                           flag_count=flag_count,
+                           start=start,
+                           end=end,
                            employee=employee)
 
 def getHours(user_id, first_day_search, last_day_search):
