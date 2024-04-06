3f97a0c6c141e8633db8027ad44a3056a8368588
Patrick Stinson <stinson6016@gmail.com>
2024-04-06 12:00:40 -0400

    admin redirect non site

--- myapp/main.py
+++ myapp/main.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, flash
 
 main = Blueprint("main", __name__)
 
 @main.route("/")
 def home ():
+    return redirect(url_for('clock.home'))
+
+@main.route('/admin/')
+def admin():
+    flash('please chech your link')
     return redirect(url_for('clock.home'))
